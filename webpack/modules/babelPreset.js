8e5d4904ea4c44cf2a73b2a3bb596d34cdd34714
Alex Masterov <alex.masterow@gmail.com>
2017-01-12 17:07:34 +0300

    Replace "babel-preset-latest" with "babel-preset-env"

--- webpack/modules/babelPreset.js
+++ webpack/modules/babelPreset.js
@@ -6,15 +6,14 @@ module.exports = (config) => {
     compact: true,
     cacheDirectory: __dirname + '/.babelCache',
     presets: [
-      [require.resolve('babel-preset-latest'), { modules: false }],
+      ['env', {
+        modules: false,
+        loose: true,
+        exclude: ['transform-regenerator']
+      }],
       require.resolve('babel-preset-react')
     ],
-    plugins: [
-      // class { handleClick = () => { } }
-      require.resolve('babel-plugin-transform-class-properties'),
-      // { ...todo, completed: true }
-      require.resolve('babel-plugin-transform-object-rest-spread')
-    ]
+    plugins: []
   };
 
   if (IS_DEV) {
