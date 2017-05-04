79c233a576ff4882f14d66cdf07dd0831482f0d0
Alex Masterov <alex.masterow@gmail.com>
2017-05-04 18:32:57 +0300

    Update url loaders configuration

--- webpack/modules/urlVideo.js
+++ webpack/modules/urlVideo.js
@@ -1,16 +1,12 @@
-const urlVideoLoader = {
-  loader: 'url-loader',
-  options: {
-    limit: 10000,
-  },
-};
-
 module.exports = (config) => {
   config.module.rules = [
     ...config.module.rules,
     {
       test: /\.(mp4|webm)$/i,
-      loader: urlVideoLoader,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+      },
     },
   ];
 
