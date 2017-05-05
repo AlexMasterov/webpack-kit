33bb303092e2ca68ac296fab75aaf05fe0d53529
Alex Masterov <alex.masterow@gmail.com>
2017-05-05 03:35:27 +0300

    Add material-ui example

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import styles from './App';
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const App = () => (
-  <div className={styles.app}>
-    <h2>Webpack Kit</h2>
-  </div>
-);
+// Material UI
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
+
+class App extends Component {
+  constructor(props) {
+    injectTapEventPlugin();
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('Clicked.');
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={lightMuiTheme}>
+        <Paper zDepth={1}>
+          <RaisedButton
+            label='Button'
+            fullWidth={true}
+            onClick={this.handleClick}
+          />
+        </Paper>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 export default App;
