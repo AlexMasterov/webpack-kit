e7ed2dbe9a52f0f7e22fbc52df76a844259a37b3
Alex Masterov <alex.masterow@gmail.com>
2017-05-06 08:00:13 +0300

    Improve Koa2 server configuration

--- webpack/env.js
+++ webpack/env.js
@@ -1,6 +1,8 @@
 const env = {
   isProd: process.env.NODE_ENV === 'production',
   isDev: process.env.NODE_ENV === 'development',
+  host: process.env.SERVER_HOST || 'localhost',
+  port: process.env.SERVER_PORT || 3000,
 };
 
 module.exports = env;
