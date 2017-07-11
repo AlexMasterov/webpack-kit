89228d9bf7074f98a2423d5e7792725788c20913
Alex Masterov <alex.masterow@gmail.com>
2017-07-11 10:28:13 +0300

    Improve Koa2 middleware configuration

--- webpack/server/middlewares/koa2.js
+++ webpack/server/middlewares/koa2.js
@@ -1,13 +1,17 @@
 const webpack = require('webpack');
 const koaMiddleware = require('koa-webpack');
-
 const cwd = process.cwd();
 
-let config = require(`${cwd}/webpack.config`);
-
 // Configure Hot Reload
-config = require(`${cwd}/webpack/plugins/hotReload`)(config);
-config = require(`${cwd}/webpack/plugins/html`)(config);
+const configs = [
+  'plugins/html',
+  'plugins/hotReload',
+];
+
+const config = configs.reduce(
+  (config, filepath) => require(`${cwd}/webpack/${filepath}`)(config),
+  require(`${cwd}/webpack.config`)
+);
 
 // Configure Webpack
 config.output.filename = config.output.filename.replace('chunkhash', 'hash');
